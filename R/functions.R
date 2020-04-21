2cb836ed130c5a2838259f8464b183fdc4ae116c
Skumin <polach.honza@gmail.com>
2020-04-21 09:29:04 +0100

    added PD TS functions

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,38 @@
 library(data.table)
 library(ggplot2)
 
+apd <- function(cpds, tenors = NULL) {
+  if(any(diff(cpds) <= 0)) {
+    stop('Cumulative default probabilities must be increasing.')
+  }
+  if(any(cpds <= 0)) {
+    stop('Cumulative default probabilities must be greater than zero.')
+  }
+  if(is.null(tenors)) {
+    tnrs <- seq_along(cpds)
+  } else {
+    tnrs <- tenors
+  }
+  return(1 - (1 - cpds)^(1/tnrs))
+}
+
+cpd <- function(apds, tenors = NULL) {
+  if(any(apds <= 0)) {
+    stop('Annualized default probabilities must be greater than zero.')
+  }
+  if(is.null(tenors)) {
+    tnrs <- seq_along(apds)
+  } else {
+    tnrs <- tenors
+  }
+  cpds <- 1 - (1 - apds)^tnrs
+  if(any(diff(cpds) <= 0)) {
+    stop('The annualized default probabilities result in non-increasing cumulative defaut probabilities.')
+  } else {
+    return(cpds)
+  }
+}
+
 binomial_test <- function(n, p, a) {
   return(head(which(pbinom(c(0, seq_len(n)), n, p) >= a) - 1, 1))
 }

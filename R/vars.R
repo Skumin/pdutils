cb90cfd5bfd9b359a6402edb605d4197b630027a
Skumin <polach.honza@gmail.com>
2020-06-25 11:41:57 +0100

    typo in MIS scale

--- R/vars.R
+++ R/vars.R
@@ -1,5 +1,4 @@
 # Variables
-
-mis_scale <- c('Aaa', 'Aa1', 'Aa2', 'Aa2', 'A1', 'A2', 'A3', 'Baa1', 'Baa2', 'Baa3', 'Ba1', 'Ba2', 'Ba3', 'B1', 'B2', 'B3', 'Caa1', 'Caa2', 'Caa3', 'Ca', 'C')
+mis_scale <- c('Aaa', 'Aa1', 'Aa2', 'Aa3', 'A1', 'A2', 'A3', 'Baa1', 'Baa2', 'Baa3', 'Ba1', 'Ba2', 'Ba3', 'B1', 'B2', 'B3', 'Caa1', 'Caa2', 'Caa3', 'Ca', 'C')
 sp_scale <- c('AAA', 'AA+', 'AA', 'AA-', 'A+', 'A', 'A-', 'BBB+', 'BBB', 'BBB-', 'BB+', 'BB', 'BB-', 'B+', 'B', 'B-', 'CCC+', 'CCC', 'CCC-', 'CC', 'C', 'D')
 fitch_scale <- c('AAA', 'AA+', 'AA', 'AA-', 'A+', 'A', 'A-', 'BBB+', 'BBB', 'BBB-', 'BB+', 'BB', 'BB-', 'B+', 'B', 'B-', 'CCC', 'DDD', 'DD', 'D')
